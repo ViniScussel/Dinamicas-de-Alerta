2697e530312be57831b558e25974e8b750807559
Scussel <85595449+ViniScussel@users.noreply.github.com>
2025-05-06 14:38:38 -0300

    Improved Foragers Movement

---
+++ source-2.4/extras/constantes.h
@@ -0,0 +1,20 @@
+#ifndef CONSTANTES_H
+#define CONSTANTES_H
+
+int SIMULATION_STEPS = 100;
+const float ALERT_THRESHOLD = 1.5f;
+const float LEARNING_RATE = 0.1f;
+const float DISCOUNT_FACTOR = 0.9f;
+const float EXPLORATION_RATE = 0.1f;
+const float FOOD_REWARD = 1.5f;
+const float FORAGE_RADIUS = 0.1f;
+const float MOVE_DISTANCE = 0.05f;
+const int ACTIONS_ARE_DAYS = 50;
+
+//USAR ESSES NUMEROS MUDAR
+const int SENTINEL_NUMBER = 10;
+const int FORRAGER_NUMBER = 10;
+const int THREAT_NUMBER = 10;
+const float DOMINANT_RANGE = 0.15f;
+
+#endif
